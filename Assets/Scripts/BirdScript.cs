7d8bec432fbbfefcfa0729d1462c35ae661bb30d
GMihalkov <georgimihalkov@abv.bg>
2020-11-29 01:03:45 +0200

    feat: implemented base game functionality

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private const float _gravityScaleAfterDeath = 3f;
+
+    public float jumpForce;
+    private bool _isDead;
+    private Rigidbody2D _rigidbody;
+
+    public bool IsDead => this._isDead;
+
+    void Start() => this._rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !this._isDead)
+        {
+            this._rigidbody.AddRelativeForce(Vector2.up * this.jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        this._isDead = true;
+        this._rigidbody.gravityScale = _gravityScaleAfterDeath;
+    }
+}
---
+++ Assets/Scripts/PipesScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class PipesScript : MonoBehaviour
+{
+    public GameObject bird;
+    public float movementSpeed;
+    private GameObject[] _pipes;
+    private BirdScript _birdScript;
+
+    void Start() 
+    {
+        this._birdScript = this.bird.GetComponent<BirdScript>();
+        this._pipes = GameObject.FindGameObjectsWithTag("Pipe");
+    }
+
+    void Update()
+    {
+        if (this._birdScript.IsDead) return;
+
+        foreach (var pipe in this._pipes)
+        {
+            pipe.transform.Translate(Vector2.left * this.movementSpeed * Time.deltaTime);
+
+            if (pipe.transform.position.x <= -5.1f)
+            {
+                var lastPipeX = this._pipes.OrderByDescending((p) => p.transform.position.x).First().transform.position.x;
+
+                var pipePos = pipe.transform.position;
+                pipePos.x = lastPipeX + 1f;
+
+                pipe.transform.position = pipePos;
+            }
+        }
+    }
+}
